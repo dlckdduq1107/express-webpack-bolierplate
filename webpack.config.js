c4bbc7e9b2469b13b01fd0a3c0466efe4a0cabd4
J165_이창엽 <dlckdduq97@gmail.com>
2021-09-07 18:12:28 +0900

    babel polyfill추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'none',
-  entry: './src/frontend/index.js',
+  entry: ['@babel/polyfill','./src/frontend/index.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -29,9 +29,11 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: "./dist",
-    overlay: true // 오류내용을 화면에 보여주게 함.
+    port:3000,
+    // contentBase: "./dist",
+    // overlay: true // 오류내용을 화면에 보여주게 함.
   },
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/frontend/index.html')
