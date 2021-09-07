506eeb506e0bc75826e95ad3e94868299018f0d3
J165_이창엽 <dlckdduq97@gmail.com>
2021-09-07 17:57:12 +0900

    프로젝트 구조 재수정
    프론트와 백엔드로 나눔

---
+++ src/frontend/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+class App {
+    constructor(parent) {
+        this.parent = parent;
+
+        this.parent.innerHTML = `<h1>Hello World</h1>`;
+    }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    new App(document.getElementById('app'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-let path = require('path');
-let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: './src/frontend/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "http://localhost:3000/dist" //웹팩 미들웨어 장소
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.m?js$/,
@@ -35,9 +33,8 @@ module.exports = {
     overlay: true // 오류내용을 화면에 보여주게 함.
   },
   plugins: [
-    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html", //html  webpack플러그인을 통해 html 파일도 함께 bundle
-    }),
+      template: path.join(__dirname, 'src/frontend/index.html')
+    })
   ],
 }
